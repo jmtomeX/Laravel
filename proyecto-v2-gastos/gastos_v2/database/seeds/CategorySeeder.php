4e09cbce0448f517e647fd7e94b5f8415dbdd265
JOSE M TOME MEJIAS <1920OPDW107@CEBANCDEA.EDU>
2020-01-27 19:38:59 +0100

    Modificando seeders

--- proyecto-v2-gastos/gastos_v2/database/seeds/CategorySeeder.php
+++ proyecto-v2-gastos/gastos_v2/database/seeds/CategorySeeder.php
@@ -15,49 +15,49 @@ class CategorySeeder extends Seeder
         $user = User::find(1);
 
         $category = new Category();
-        $category->description = 'General';
+        $category->category = 'General';
         $category->user_id = $user->id;
         $category->save();
 
         $type = new Type();
-        $type->description = 'General';
+        $type->type = 'General';
         $type->category_id = $category->id;
         $type->save();
 
         $category = new Category();
-        $category->description = 'Hogar';
+        $category->category = 'Hogar';
         $category->user_id = $user->id;
         $category->save();
 
         $type = new Type();
-        $type->description = 'Luz';
+        $type->type = 'Luz';
         $type->category_id = $category->id;
         $type->save();
 
         $type = new Type();
-        $type->description = 'Gas';
+        $type->type = 'Gas';
         $type->category_id = $category->id;
         $type->save();
 
         //********************* */
 
         $category = new Category();
-        $category->description = 'Seguros';
+        $category->category = 'Seguros';
         $category->user_id = $user->id;
         $category->save();
 
         $type = new Type();
-        $type->description = 'Casa';
+        $type->type = 'Casa';
         $type->category_id = $category->id;
         $type->save();
 
         $type = new Type();
-        $type->description = 'Coche';
+        $type->type = 'Coche';
         $type->category_id = $category->id;
         $type->save();
 
         $type = new Type();
-        $type->description = 'Moto';
+        $type->type = 'Moto';
         $type->category_id = $category->id;
         $type->save();
 
@@ -66,49 +66,49 @@ class CategorySeeder extends Seeder
         $user = User::find(2);
 
         $category = new Category();
-        $category->description = 'PeterGeneral';
+        $category->category = 'PeterGeneral';
         $category->user_id = $user->id;
         $category->save();
 
         $type = new Type();
-        $type->description = 'PeterGeneral';
+        $type->type = 'PeterGeneral';
         $type->category_id = $category->id;
         $type->save();
 
         $category = new Category();
-        $category->description = 'PeterHogar';
+        $category->category = 'PeterHogar';
         $category->user_id = $user->id;
         $category->save();
 
         $type = new Type();
-        $type->description = 'PeterLuz';
+        $type->type = 'PeterLuz';
         $type->category_id = $category->id;
         $type->save();
 
         $type = new Type();
-        $type->description = 'PeterGas';
+        $type->type = 'PeterGas';
         $type->category_id = $category->id;
         $type->save();
 
         //********************* */
 
         $category = new Category();
-        $category->description = 'PeterSeguros';
+        $category->category = 'PeterSeguros';
         $category->user_id = $user->id;
         $category->save();
 
         $type = new Type();
-        $type->description = 'PeterCasa';
+        $type->type = 'PeterCasa';
         $type->category_id = $category->id;
         $type->save();
 
         $type = new Type();
-        $type->description = 'PeterCoche';
+        $type->type = 'PeterCoche';
         $type->category_id = $category->id;
         $type->save();
 
         $type = new Type();
-        $type->description = 'PeterMoto';
+        $type->type = 'PeterMoto';
         $type->category_id = $category->id;
         $type->save();
     }
